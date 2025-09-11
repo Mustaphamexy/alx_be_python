42e708514ae22a9bca2738c43a82b45eee8a2de2
Mustaphamexy <mustaphamexy111@gmail.com>
2025-09-11 21:00:15 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+print("assuming number1 =", number1, "and number2 =", number2)
+sum = number1 + number2
+print("Sum of", number1, "and", number2, "is", sum)
+difference = number1 - number2
+print("Difference of", number2, "and", number1, "is", difference)
+product = number1 * number2
+print("Product of", number1, "and", number2, "is", product)
